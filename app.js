f2be71975eeee285b1421c528bb3e83f678d080f
SantiDani <profsantiRP@gmail.com>
2021-08-03 18:45:08 +0200

    Middleware - Index and 404

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const {response} = require("express");
+const path = require("path");
 const app = express();
 const port = 5000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/',
-    (req, res) => res.send('Hello World!'));
+    (req, res) => {
+    res.status(200).sendFile(__dirname + '/films/index.html');
+    });
 
 // Routing
-app.get('/film',
-    (req, res) => res.send('Ruta film'));
+// The order is important.
+app.get('/url-code',
+    (req, res) => {
+        res.send(`Esta es la ruta en al que se ejecuta el codigo ${__dirname} `);
+    });
+
+app.use((req, res, next) => {
+   res.status(404).sendFile(__dirname + '/films/404.html')
+});
 
 app.listen(port, ()=> console.log(`Express is listening at http://localhost/${port}`));
 
