75b9cedf1c2876bbd3457d7530e3d04f611cf0ab
SantiDani <profsantiRP@gmail.com>
2021-08-03 18:14:03 +0200

    Routing

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const port = 5000;
 app.get('/',
     (req, res) => res.send('Hello World!'));
 
+// Routing
+app.get('/film',
+    (req, res) => res.send('Ruta film'));
+
 app.listen(port, ()=> console.log(`Express is listening at http://localhost/${port}`));
 
 
