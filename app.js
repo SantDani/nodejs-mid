3f624c0a11d6fce36197f972274bf50957f8abe1
SantiDani <profsantiRP@gmail.com>
2021-08-03 18:11:35 +0200

    Server HTTP - Express

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const http = require("http");
-const port = process.env.PORT || 5000;
+const express = require('express');
+const {response} = require("express");
+const app = express();
+const port = 5000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end("<h1>Hello, World!</h1>");
-});
+app.get('/',
+    (req, res) => res.send('Hello World!'));
 
-server.listen(port, () => console.log(`Server running at port ${port}`));
+app.listen(port, ()=> console.log(`Express is listening at http://localhost/${port}`));
+
+
