48228a781fce6fc437eee41f8540f21305e8e1b2
SantiDani <profsantiRP@gmail.com>
2021-08-10 17:23:52 +0200

    await / sync in app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const mongoFilms = require('./models/connectMongoDB');
+const util = require('util');
+const mongoDB = require('./models/connectMongoDB');
 const fs = require('fs');
+const mongoose = require("mongoose");
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+
 
 
 // Main Templates - EJS
@@ -22,4 +27,29 @@ app.use('/', require('./router/routerPaths'));
 app.listen(PORT, ()=> console.log(`Express is listening at http://localhost:${PORT}`));
 
 // https://nodejs-mid.herokuapp.com/
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster-free.5gk0p.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(uri,
+            {
+                useNewUrlParser: true ,
+                useUnifiedTopology: true,
+                keepAlive: true,
+                useFindAndModify: true
+            })
+            .then(( result) => {
+                console.log('Log to Database name: ');
+                // console.log(`${result}`);
+                console.log(`${result.connections[0].name}`);
+            })
+            .catch(e => console.error('error login in MongoDB ', e));
+    }catch (e){
+        console.error(e);
+    }
+};
+
+connectDB().then(r => {
+    console.log('ConnectDB')});
+
 
