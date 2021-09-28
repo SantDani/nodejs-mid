7b6edd1dde0b0c8de4dac84e3749bfaa9216be51
SantiDani <profsantiRP@gmail.com>
2021-09-28 23:37:43 +0200

    fix navbar with bootstrap 4.5

--- router/routerPaths.js
+++ router/routerPaths.js
@@ -4,20 +4,20 @@ const Film = require('./../models/film');
 const {request, response} = require("express");
 
 router.get('/', async (request, response) => {
-   response.render('index', {title : 'Title Home'});
+   response.render('films', {title : 'Title Home'});
     console.log('Someone is connect to Home');
 
 });
 
 router.get('/nosotros', (req, res) => {
-    res.render('about', {title: 'Nosotros EJS'});
+    res.render('about', {title: 'About us EJS'});
     console.log('Someone is connect to about');
 });
 
 router.get('/contacto', (req, res) => {
 
-    res.render('contact', {title: 'Contacto EJS'});
-    // console.log('Someone is connect to Contact');
+    res.render('contact', {title: 'Contact EJS'});
+    console.log('Someone is connect to Contact');
 
 });
 
